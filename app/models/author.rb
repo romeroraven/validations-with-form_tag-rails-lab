53cfd7097cf93d75637d76b105a389bdc43fca2d
romeroraven <romeroravenr@gmail.com>
2021-02-15 11:30:03 -0700

    commit tree 948a95c947e72955cda4f78d51ed2c7ec26cacf1

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+    validates :name, length: { minimum: 2 }
+    validates :phone_number, length: { is: 10 }
+    validates :email, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    belongs_to :author
+    validates :title, length: { minimum: 2 }
+    validates :content, length: { minimum: 200 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
